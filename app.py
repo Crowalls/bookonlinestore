3d6da1a751bfe3a175c8da4ae8fdc50c7fca1879
Crowalls <141907850+Crowalls@users.noreply.github.com>
2024-06-02 21:09:15 +0800

    app.py

--- app.py
+++ app.py
@@ -32,7 +32,13 @@ def create_app():
             db.session.add(admin)
             db.session.commit()
 
-    from blueprints import register_bp, login_bp, book_bp, cart_bp, profile_bp, order_bp, admin_bp
+    from blueprints.register_bp import register_bp
+    from blueprints.login_bp import login_bp
+    from blueprints.book_bp import book_bp
+    from blueprints.cart_bp import cart_bp
+    from blueprints.profile_bp import profile_bp
+    from blueprints.order_bp import order_bp
+    from blueprints.admin_bp import admin_bp
 
     app.register_blueprint(register_bp, url_prefix='/')
     app.register_blueprint(login_bp, url_prefix='/')
@@ -40,7 +46,7 @@ def create_app():
     app.register_blueprint(cart_bp, url_prefix='/cart')
     app.register_blueprint(profile_bp, url_prefix='/')
     app.register_blueprint(order_bp, url_prefix='/')
-    app.register_blueprint(admin_bp, url_prefix='/')
+    app.register_blueprint(admin_bp, url_prefix='/admin')
 
     @app.route('/')
     def main():
