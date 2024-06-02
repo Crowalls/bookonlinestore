b17e753e164b22cf91b43718063931ee51d8a66e
Crowalls <141907850+Crowalls@users.noreply.github.com>
2024-06-02 19:31:55 +0800

    models.py

--- models.py
+++ models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(20))
     register_date = db.Column(db.DateTime, default=db.func.current_timestamp())
 
@@ -54,3 +54,6 @@ class CartItem(db.Model):
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     book = db.relationship('Book')
+
+    def __repr__(self):
+        return f'<CartItem {self.id}>'
