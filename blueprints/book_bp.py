2fcd9ef0a3072e2fae16fc6dc343a4b09df4939e
Crowalls <141907850+Crowalls@users.noreply.github.com>
2024-06-02 20:43:12 +0800

    book_bp.py

--- blueprints/book_bp.py
+++ blueprints/book_bp.py
@@ -1,17 +1,63 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required
-from models import Book
+from models import db, Book
+from decimal import Decimal
 
 book_bp = Blueprint('book', __name__, template_folder='../templates')
 
-@book_bp.route('/books', methods=['GET'])
+@book_bp.route('/books')
 @login_required
-def books():
-    all_books = Book.query.all()
-    return render_template('book_detail.html', books=all_books)
+def list_books():
+    books = Book.query.all()
+    books_list = [{
+        'id': book.id,
+        'title': book.title,
+        'author': book.author,
+        'price': float(book.price),
+        'description': book.description,
+        'stock': book.stock,
+        'cover_image': book.cover_image
+    } for book in books]
+    return jsonify(books_list)
 
-@book_bp.route('/book/<int:book_id>', methods=['GET'])
+@book_bp.route('/books/add', methods=['POST'])
 @login_required
-def book_detail(book_id):
-    book = Book.query.get_or_404(book_id)
-    return render_template('book_detail.html', book=book)
+def add_book():
+    data = request.json
+    new_book = Book(
+        title=data['title'],
+        author=data['author'],
+        price=Decimal(data['price']),
+        description=data['description'],
+        stock=data['stock'],
+        cover_image=data.get('cover_image')
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return '', 201
+
+@book_bp.route('/books/edit/<int:book_id>', methods=['POST'])
+@login_required
+def edit_book(book_id):
+    data = request.json
+    book = Book.query.get(book_id)
+    if book:
+        book.title = data['title']
+        book.author = data['author']
+        book.price = Decimal(data['price'])
+        book.description = data['description']
+        book.stock = data['stock']
+        book.cover_image = data['cover_image']
+        db.session.commit()
+        return '', 204
+    return '', 404
+
+@book_bp.route('/books/delete/<int:book_id>', methods=['DELETE'])
+@login_required
+def delete_book(book_id):
+    book = Book.query.get(book_id)
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        return '', 204
+    return '', 404
