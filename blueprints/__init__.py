a5819ab18d03a36e50da3617c443f4934d752fa8
Crowalls <141907850+Crowalls@users.noreply.github.com>
2024-06-02 20:40:49 +0800

    __init__.py

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,21 +1,9 @@
-# __init__.py
+from .register_bp import register_bp
+from .login_bp import login_bp
+from .book_bp import book_bp
+from .cart_bp import cart_bp
+from .profile_bp import profile_bp
+from .order_bp import order_bp
+from .admin_bp import admin_bp
 
-from flask import Blueprint
-
-# 初始化各个蓝图
-register_bp = Blueprint('register', __name__)
-login_bp = Blueprint('login', __name__)
-book_bp = Blueprint('book', __name__)
-cart_bp = Blueprint('cart', __name__)
-profile_bp = Blueprint('profile', __name__)
-order_bp = Blueprint('order', __name__)
-admin_bp = Blueprint('admin', __name__)
-
-# 导入各个蓝图的路由模块
-from . import register_bp_routes
-from . import login_bp_routes
-from . import book_bp_routes
-from . import cart_bp_routes
-from . import profile_bp_routes
-from . import order_bp_routes
-from . import admin_bp_routes
+__all__ = ['register_bp', 'login_bp', 'book_bp', 'cart_bp', 'profile_bp', 'order_bp', 'admin_bp']
