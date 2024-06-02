edd8bb307281ce1886ab4091e0066c4883d26c2c
Crowalls <141907850+Crowalls@users.noreply.github.com>
2024-06-02 21:00:59 +0800

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://onlinebookstore_db_user:ymqtgnenCJKAXtzLD1RHptyWz6xZ1MAH@dpg-cpe2945ds78s73epafbg-a/onlinebookstore')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('DATABASE_URL') or 
+        'postgresql://onlinebookstore_db_user:yqmtgnenCJKAXtzLD1RHptyWz6xZ1MAH@dpg-cpe2945ds78s73epafbg-a:5432/onlinebookstore_db'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
