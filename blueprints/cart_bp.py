de6b235a16f738a79cf2a94dd04db27b5d771cf5
Crowalls <141907850+Crowalls@users.noreply.github.com>
2024-06-02 23:18:39 +0800

    cart_bp.py

--- blueprints/cart_bp.py
+++ blueprints/cart_bp.py
@@ -1,43 +1,41 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from models import db, CartItem, Book
 
-cart_bp = Blueprint('cart', __name__, template_folder='../templates')
+cart_bp = Blueprint('cart_bp', __name__, template_folder='../templates')
 
-@cart_bp.route('/')
+@cart_bp.route('/cart')
 @login_required
 def view_cart():
     cart_items = CartItem.query.filter_by(user_id=current_user.id).all()
-    cart_list = [{
-        'id': item.id,
-        'book_id': item.book_id,
-        'title': item.book.title,
-        'quantity': item.quantity
-    } for item in cart_items]
-    return jsonify(cart_list)
+    return render_template('cart.html', cart_items=cart_items)
 
-@cart_bp.route('/add', methods=['POST'])
+@cart_bp.route('/cart/add/<int:book_id>')
 @login_required
-def add_to_cart():
-    data = request.json
-    book = Book.query.get(data['book_id'])
-    if book:
-        cart_item = CartItem.query.filter_by(user_id=current_user.id, book_id=book.id).first()
-        if cart_item:
-            cart_item.quantity += 1
-        else:
-            cart_item = CartItem(user_id=current_user.id, book_id=book.id, quantity=1)
-            db.session.add(cart_item)
-        db.session.commit()
-        return '', 201
-    return '', 404
+def add_to_cart(book_id):
+    existing_item = CartItem.query.filter_by(user_id=current_user.id, book_id=book_id).first()
+    if existing_item:
+        existing_item.quantity += 1
+    else:
+        new_item = CartItem(user_id=current_user.id, book_id=book_id, quantity=1)
+        db.session.add(new_item)
+    db.session.commit()
+    return redirect(url_for('cart_bp.view_cart'))
 
-@cart_bp.route('/remove/<int:cart_item_id>', methods=['DELETE'])
+@cart_bp.route('/cart/remove/<int:cart_item_id>')
 @login_required
 def remove_from_cart(cart_item_id):
-    cart_item = CartItem.query.get(cart_item_id)
-    if cart_item:
+    cart_item = CartItem.query.get_or_404(cart_item_id)
+    if cart_item.user_id == current_user.id:
         db.session.delete(cart_item)
         db.session.commit()
-        return '', 204
-    return '', 404
+    return redirect(url_for('cart_bp.view_cart'))
+
+@cart_bp.route('/cart/update/<int:cart_item_id>', methods=['POST'])
+@login_required
+def update_cart_item(cart_item_id):
+    cart_item = CartItem.query.get_or_404(cart_item_id)
+    if cart_item.user_id == current_user.id:
+        cart_item.quantity = request.form.get('quantity', type=int)
+        db.session.commit()
+    return redirect(url_for('cart_bp.view_cart'))
